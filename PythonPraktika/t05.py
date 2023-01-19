4dd01816489e8776ffa11e8ae3d236c08b3faeac
BalliMammadli <116656794+BalliMammadli@users.noreply.github.com>
2023-01-19 18:33:24 +0400

    Update t05.py

--- PythonPraktika/t05.py
+++ PythonPraktika/t05.py
@@ -1,7 +1,16 @@
 # Verilən massivdəki ən böyük ədədi tapın.
 
 def findMax(lst):
-    pass
-    # pass ifadesini silin və öz kodunuzu yazın
+    def findMax(lst):
+   i=0
+   for i in lst:
     
-findMax([1, 2, 3]) # 3
+    if lst[i]>lst[i+1]:
+        return lst[i]
+    else:
+        return lst[i+1]
+        
+  
+
+    
+print(findMax([1, 2, 3])) # 3
