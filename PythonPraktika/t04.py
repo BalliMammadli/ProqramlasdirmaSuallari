dbda72b7b193368cfc3bcd37d07e8a152ede6422
BalliMammadli <116656794+BalliMammadli@users.noreply.github.com>
2023-01-19 18:32:36 +0400

    Update t04.py

--- PythonPraktika/t04.py
+++ PythonPraktika/t04.py
@@ -1,7 +1,15 @@
 # Verilən ədədin rəqəmlərini tərsinə çevirən funksiya yazın.
 
 def reverseNumberDigits(a):
-    pass
-    # pass ifadesini silin və öz kodunuzu yazın
+n=int(input("ededi daxil edin: "))
+sum=0
+def findSumOfDigits(a):
+    global n
+    global sum
+    while n>0:
+        r=n%10
+        n=n//10
+        print(r)
+
     
-reverseNumberDigits(123) # 321
+reverseNumberDigits(123) # 321 #eyni setirde nece edirdik unutdum
