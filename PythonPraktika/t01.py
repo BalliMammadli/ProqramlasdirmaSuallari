d966184c9dd1dff7fb6f08f1c0a85ca0151adc6b
BalliMammadli <116656794+BalliMammadli@users.noreply.github.com>
2023-01-19 18:28:37 +0400

    Update t01.py

--- PythonPraktika/t01.py
+++ PythonPraktika/t01.py
@@ -1,8 +1,13 @@
 # Verilən 3 ədəd arasında ən böyük ədədi tapın.
 
 def findMax(a, b, c):
-    pass
-    # pass ifadesini silin və öz kodunuzu yazın
+    if a>b and a>c:
+        print(a)
+    elif b>a and b>c:
+        print(b)
+    else:
+        print(c)
+
 
 
 findMax(1, 2, 3) # 3
