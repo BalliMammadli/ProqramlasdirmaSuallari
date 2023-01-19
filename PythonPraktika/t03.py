e6c8e063a2f1713c6435d6026fb3a90ab9d280c7
BalliMammadli <116656794+BalliMammadli@users.noreply.github.com>
2023-01-19 18:31:29 +0400

    Update t03.py

--- PythonPraktika/t03.py
+++ PythonPraktika/t03.py
@@ -1,7 +1,16 @@
 # Bir a ədədi verilir.Sizdən tələb olunan bu ədədin rəqəmlərinin cəmini tapmadır
 
 def findSumOfDigits(a):
-    pass
-    # pass ifadesini silin və öz kodunuzu yazın
+n=int(input("ededi daxil edin: "))
+sum=0
+def findSumOfDigits(a):
+    global n
+    global sum
+    while n>0:
+        r=n%10
+        n=n//10
+        sum=sum+r
+    return sum
+
     
 findSumOfDigits(123) # 6
